0a1e030dbfd2e92951d1e3371ab9bac22416bf27
s-eunbi-kim <86344770+s-eunbi-kim@users.noreply.github.com>
2024-11-29 17:05:30 +0900

    Create checkSpeed.h

---
+++ src/checkSpeed.h
@@ -0,0 +1,11 @@
+#ifndef HIKER_INCLUDED
+#define HIKER_INCLUDED
+
+#include <stdbool.h>
+
+bool isInRange(int, int, int);
+bool checkHigh(int);
+bool checkLow(int);
+const char* displayStatus(int, int, int);
+
+#endif
