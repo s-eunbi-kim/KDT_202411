898d079c2a5f5111875b1c90d8c4826c082c3fa0
s-eunbi-kim <86344770+s-eunbi-kim@users.noreply.github.com>
2024-11-29 16:07:50 +0900

    Create calc.h

---
+++ src/calc.h
@@ -0,0 +1,12 @@
+#ifndef HIKER_INCLUDED
+#define HIKER_INCLUDED
+#include <stdbool.h>
+
+bool isPositive(int);
+bool arePositive(int, int);
+int add(int, int);
+int subtract(int, int);
+int divide(int, int);
+int multiply(int, int);
+
+#endif
